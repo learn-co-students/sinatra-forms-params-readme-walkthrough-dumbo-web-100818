ccf2e752628c941f1d59f0d8bd0f34b44a70f206
achadha2 <andy.chadha2@gmail.com>
2018-10-30 13:40:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,10 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do
-    params = {
-      :name => "Sam",
-      :favorite_food => "Green Eggs and Ham"
-    }
+    
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
   # Add your post route and action below
